b11a0608e6c648a01f7b96ca214b03aa3796e335
XoriieInpottn <XoriieInpottn@126.com>
2025-01-09 16:48:10 +0800

    add dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
             'libentry',
             "libentry.service"
         ],
-        version='1.18',
+        version='1.18.1',
         description='Entries for experimental utilities.',
         long_description_content_type='text/markdown',
         long_description=long_description,
@@ -30,6 +30,7 @@ if __name__ == '__main__':
         install_requires=[
             "requests",
             "pydantic",
+            "numpy",
             "json5",
             "PyYAML",
             "Flask",
